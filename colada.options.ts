fd51b9de69f5f2072d174caee1cfa61bdbb410ac
Javier Puche <javierpuchevictoria@gmail.com>
2026-03-05 11:42:58 +0100

    chore: remove colada.options.ts

--- colada.options.ts
+++
@@ -1,8 +0,0 @@
-import type { PiniaColadaOptions } from '@pinia/colada'
-
-export default {
-  query: {
-    staleTime: 1000 * 60 * 5, // 5 minutes
-    gcTime: 1000 * 60 * 30, // 30 minutes
-  },
-} satisfies PiniaColadaOptions
